f1ffef6ea88bf709b51b1bff2e944a372e795f48
Rolando Cruz <rolandocrzprz@gmail.com>
2025-04-03 22:04:32 -0700

    Primer Commit | Estructura base (basica)

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+// Crear instancia express
+const app = express();
+
+// Politicas CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  // Manejar preflight requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+// ruta raiz
+app.get("/", (req, res) => {
+  res.send("Probando");
+});
+
+// Escuchar Puerto
+app.listen(3000, () => {
+  console.log("Servidor escuchando en el puerto 3000");
+});
