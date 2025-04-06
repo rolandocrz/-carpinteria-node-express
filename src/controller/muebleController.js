5825197339a9c4f430f7376bddb226680c11f73b
Rolando Cruz <rolandocrzprz@gmail.com>
2025-04-06 00:18:49 -0700

    Se realizaron las rutas de los producto (muebles) y se agregaron las funciones del controlador sin logica.

--- src/controller/muebleController.js
+++ src/controller/muebleController.js
@@ -1,1 +1,22 @@
 // Controlador de productos (muebles)
+
+const getMuebles = async (req, res) => {};
+
+const getMueble = async (req, res) => {};
+
+const getMueblesPorCategoria = async (req, res) => {};
+
+const createMueble = async (req, res) => {};
+
+const updateMueble = async (req, res) => {};
+
+const deleteMueble = async (req, res) => {};
+
+export {
+  getMuebles,
+  getMueble,
+  getMueblesPorCategoria,
+  createMueble,
+  updateMueble,
+  deleteMueble,
+};
