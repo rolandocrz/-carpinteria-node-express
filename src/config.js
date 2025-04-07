8b42eb52b90ee93717615f7b7d15ab4f48b9d3b5
Rolando Cruz <rolandocrzprz@gmail.com>
2025-04-07 11:12:08 -0700

    Configurando db para prubeas de desarrollo

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ import { config } from "dotenv";
 config();
 
 const PORT = process.env.PORT || 3000;
-const DB_PORT = process.env.DB_PORT || 3306;
+const DB_PORT = process.env.DB_PORT || 22109;
 const DB_USER = process.env.DB_USER || "root";
-const DB_PASS = process.env.DB_PASS || "crzpass";
-const DB_DATABASE = process.env.DB_DATABASE || "sistemas";
-const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_PASS = process.env.DB_PASS || "LheydpjedyqIcWLEIKvPiJXWFObrsmNS";
+const DB_DATABASE = process.env.DB_DATABASE || "railway";
+const DB_HOST = process.env.DB_HOST || "caboose.proxy.rlwy.net";
 
 export { PORT, DB_PORT, DB_USER, DB_PASS, DB_DATABASE, DB_HOST };
